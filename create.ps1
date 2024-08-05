c05a05307c1b0d13cb7894deb42ac22ae486e6d5
Anılcan Kara <ab186100@adalet.gov.tr>
2024-08-05 17:24:07 +0300

    local domain

--- create.ps1
+++ create.ps1
@@ -1,5 +1,5 @@
 $params = @{
-  DnsName = "Your Root Cert"
+  DnsName = "Anilcan Root Cert"
   KeyLength = 2048
   KeyAlgorithm = 'RSA'
   HashAlgorithm = 'SHA256'
@@ -12,7 +12,7 @@ $params = @{
 $rootCA = New-SelfSignedCertificate @params
 
 $params = @{
-  DnsName = "yourdomain.org"
+  DnsName = "anilcan.local"
   Signer = $rootCA
   KeyLength = 2048
   KeyAlgorithm = 'RSA'
@@ -25,18 +25,18 @@ $params = @{
 $newCert = New-SelfSignedCertificate @params
 
 # Extra step needed since self-signed cannot be directly shipped to trusted root CA store
-# if you want to silence the cert warnings on other systems you'll need to import the rootCA.crt on them too
-Export-Certificate -Cert $rootCA -FilePath "C:\certs\rootCA.crt"
-Export-Certificate -Cert $newCert -FilePath "C:\certs\newcert.crt"
-Import-Certificate -CertStoreLocation 'Cert:\LocalMachine\Root' -FilePath "C:\certs\rootCA.crt"
+# if you want to silence the cert warnings on other systems you'll need to import the anilcan.local.root.crt on them too
+Export-Certificate -Cert $rootCA -FilePath "C:\certs\anilcan.local.root.crt"
+Export-Certificate -Cert $newCert -FilePath "C:\certs\anilcan.local.crt"
+Import-Certificate -CertStoreLocation 'Cert:\LocalMachine\Root' -FilePath "C:\certs\anilcan.local.root.crt"
 
-Export-PfxCertificate -Cert $newCert -FilePath 'C:\certs\newcert.pfx' -Password (ConvertTo-SecureString -AsPlainText 'YOUR_SECRET_PASSWORD' -Force)
+Export-PfxCertificate -Cert $newCert -FilePath 'C:\certs\anilcan.local.pfx' -Password (ConvertTo-SecureString -AsPlainText 'd3Dgd86uc' -Force)
 
 # convert root ca certificate to pem format
-openssl x509 -inform DER -in rootCA.crt -out rootCA.pem
+openssl x509 -inform DER -in anilcan.local.root.crt -out anilcan.local.root.pem
 
 # export server private key to pem format
-openssl pkcs12 -in newcert.pfx -nocerts -nodes -out newcertkey.pem -passin pass:YOUR_SECRET_PASSWORD
+openssl pkcs12 -in anilcan.local.pfx -nocerts -nodes -out anilcan.local.key.pem -passin pass:d3Dgd86uc
 
 # convert server certificate to pem format
-openssl x509 -inform DER -in newcert.crt -out newcert.pem
+openssl x509 -inform DER -in anilcan.local.crt -out anilcan.local.pem
